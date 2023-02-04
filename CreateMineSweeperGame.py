0ca3c952a693103041edba5ca5188df27e0924df
Sophie Nguyen <123401167+SophieNguyen113@users.noreply.github.com>
2023-02-04 13:38:35 -0500

    Add files via upload

---
+++ CreateMineSweeperGame.py
@@ -0,0 +1,146 @@
+# Prolog
+# Author:  Sophie Nguyen
+# Email:  nnguyen177@student.gsu.edu
+
+'''
+    Minesweeper is a puzzle game which the player will select a cell in a square grid continuously. 
+    Each cell hides a bomb or a value which displays the number of bombs in its neighboring cells. 
+    Just to make it clear, neighboring cells is defined as adjacent horizontally, vertically or diagonally.
+    If the player selects a cell which contains the bomb, the player loses the game.
+'''
+
+# Below is Python code for input/output
+import sys
+
+# For getting input from input.txt file
+sys.stdin = open('MineSweeperGame/input.txt', 'r')
+
+# Printing the Output to output.txt file
+sys.stdout = open('MineSweeperGame/output.txt', 'w')
+
+import random
+
+def GenerateMineSweeperBoard(n, k):
+
+    arr = [[0 for row in range(n)] for column in range(n)]
+
+    for num in range(k):
+
+        x = random.randint(0,n-1)
+        y = random.randint(0,n-1)
+        arr[y][x] = 'X'
+
+        if (x >=0 and x <= n-2) and (y >= 0 and y <= n-1):
+            if arr[y][x+1] != 'X':
+                arr[y][x+1] += 1 # center right
+
+        if (x >=1 and x <= n-1) and (y >= 0 and y <= n-1):
+            if arr[y][x-1] != 'X':
+                arr[y][x-1] += 1 # center left
+
+        if (x >= 1 and x <= n-1) and (y >= 1 and y <= n-1):
+            if arr[y-1][x-1] != 'X':
+                arr[y-1][x-1] += 1 # top left
+ 
+        if (x >= 0 and x <= n-2) and (y >= 1 and y <= n-1):
+            if arr[y-1][x+1] != 'X':
+                arr[y-1][x+1] += 1 # top right
+
+        if (x >= 0 and x <= n-1) and (y >= 1 and y <= n-1):
+            if arr[y-1][x] != 'X':
+                arr[y-1][x] += 1 # top center
+ 
+        if (x >=0 and x <= n-2) and (y >= 0 and y <= n-2):
+            if arr[y+1][x+1] != 'X':
+                arr[y+1][x+1] += 1 # bottom right
+
+        if (x >= 1 and x <= n-1) and (y >= 0 and y <= n-2):
+            if arr[y+1][x-1] != 'X':
+                arr[y+1][x-1] += 1 # bottom left
+
+        if (x >= 0 and x <= n-1) and (y >= 0 and y <= n-2):
+            if arr[y+1][x] != 'X':
+                arr[y+1][x] += 1 # bottom center
+
+    return arr
+
+def GeneratePlayerBoard(n):
+    arr = [['-' for row in range(n)] for column in range(n)]
+    return arr
+
+def DisplayBoard(map):
+    for row in map:
+        print(" ".join(str(cell) for cell in row))
+        print("")
+
+def CheckWon(map):
+    for row in map:
+        for cell in row:
+            if cell == '-':
+                return False
+    return True
+
+def CheckContinueGame(score):
+    print("Your score: ", score)
+    isContinue = input("Do you want to try again (y/n)?: ")
+    if isContinue == 'n':
+        return False
+    return True
+
+def Game():
+
+    GameStatus = True
+    while GameStatus:
+
+        '''
+        Beginner (grid size n=5; no. of bombs k=3)
+        Intermediate (grid size n=6; no. of bombs k=8)
+        Expert (grid size n=8; no. of bombs k=20)
+        '''
+        
+        difficulty = input("Select your difficulty (b, i, h): ")
+        if difficulty.lower() == 'b':
+            n = 5
+            k = 3
+        elif difficulty.lower() == 'i':
+            n = 6
+            k = 8
+        else:
+            n = 8
+            k = 20
+ 
+        minesweeper_map = GenerateMineSweeperBoard(n, k)
+        player_map = GeneratePlayerBoard(n)
+
+        score = 0
+        while True:
+            if CheckWon(player_map) == False:
+                print("Enter your cell you want to open: ")
+                x = input("X (1 to 5): ")
+                y = input("Y (1 to 5): ")
+                x = int(x) - 1 # 0 based indexing
+                y = int(y) - 1 # 0 based indexing 
+
+                if (minesweeper_map[y][x] == 'X'):
+                    print("Game Over!")
+                    DisplayBoard(minesweeper_map)
+                    GameStatus = CheckContinueGame(score)
+                    break
+
+                else:
+                    player_map[y][x] = minesweeper_map[y][x]
+                    DisplayBoard(player_map)
+                    score += 1
+ 
+            else:
+                DisplayBoard(player_map)
+                print("You have Won!")
+                GameStatus = CheckContinueGame(score)
+                break
+
+# Start of Program
+if __name__ == "__main__":
+    try:
+        Game()
+    except KeyboardInterrupt:
+        print('\nEnd of Game. Bye Bye!')
